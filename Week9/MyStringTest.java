f3b42718394a8ffc77d330c97d42f90591cfe2e6
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-01-06 17:31:41 -0500

    Add files via upload

---
+++ Week9/MyStringTest.java
@@ -0,0 +1,37 @@
+/*
+ *
+ * Student Jaime Rodriguez
+ * Bellevue University
+ *
+ */
+/* File MyStringTest.java */
+/*
+ * Assignment 9.4
+ * Purpose: Create a program that will turn an user input string
+ * and return the string in reverse order
+ *
+ * To compile the program:
+ * javac MyStringTest.java
+ *
+ * To execute the program:
+ * java MyStringTest
+ *
+ */
+import java.util.Scanner;
+
+public class MyStringTest {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //Enter input
+        System.out.print("Enter string:");
+        String stringWord = input.nextLine();
+
+        System.out.print("The Reverse order is:");
+        //Display reversed string reverse using required length() & charAt()
+        for (int i = stringWord.length() - 1; i >= 0; i--)
+            System.out.print(stringWord.charAt(i));
+    }
+}
